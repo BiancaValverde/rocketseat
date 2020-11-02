07da85bc46069b3ab716eaf0f28420c1e549217f
bianca <biancavalverde@hotmail.com>
2020-11-01 21:03:05 -0300

    Alteração no código

--- main.js
+++ main.js
@@ -1,24 +1,6 @@
-var minhaPromise = function() {
-    return new Promise(function(resolve, reject) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'https://api.github.com/users/diego3g');
-        xhr.send(null);
-
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    resolve(JSON.parse(xhr.responseText));
-                } else {
-                    reject('Erro na requisição');
-                }
-            }
-        }
-    });
-}
-
-minhaPromise()
+axios.get('https://api.github.com/users/diego3g')
  .then(function(response) {
-     console.log(response);
+     console.log(response.data.avatar_url);
  })
  .catch(function(error) {
      console.warn(error);
